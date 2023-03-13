bb475fb052b99ffe2029cc0ecf75a3667a04e9a5
hanzzakino <emperornezl50@gmail.com>
2023-03-13 23:33:56 +0800

    No Menu Bar

--- public/electron.js
+++ public/electron.js
@@ -13,6 +13,7 @@ function createWindow() {
         height: 600,
         webPreferences: { nodeIntegration: true, contextIsolation: false },
     })
+    mainWindow.setMenuBarVisibility(false)
     // and load the index.html of the app.
     console.log(__dirname)
     mainWindow.loadFile(path.join(__dirname, '../build/index.html'))
